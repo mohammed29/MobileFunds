b63d9693fb2e1adcac7c39f8a0219d99bfd2d0ac
mohammed <muslem568@gmail.com>
2014-09-08 15:12:57 +0300

    update

--- app/src/main/java/com/money/blackx/mobilefunds/Main.java
+++ app/src/main/java/com/money/blackx/mobilefunds/Main.java
@@ -67,6 +67,9 @@ public class Main extends Activity
             case 3:
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                break;
         }
     }
 
